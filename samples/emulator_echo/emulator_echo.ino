1b0c1ed2d09c87cbd358e4e2c0a39930f0e07e00
Alexander Lay <alexanderqlay@gmail.com>
2025-10-16 18:54:48 -0400

    samples: modify emulator sample to use UART2
    Signed-off-by: Alexander Lay <alexanderqlay@gmail.com>

--- samples/emulator_echo/emulator_echo.ino
+++ samples/emulator_echo/emulator_echo.ino
@@ -1,13 +1,19 @@
+HardwareSerial downlinkSerial(2);
+
 void setup() {
-  Serial.begin(115200);  /* USB Serial (UART0) */
-  while (!Serial);
+  Serial.begin(115200);  /* USB Serial (UART0) for debug printing */
+  downlinkSerial.begin(115200, SERIAL_8N1, 16, 17); /* UART2 using GPIO pins 16, 17 */
 }
 
 void loop() {
-  if (Serial.available()) {
-    String input = Serial.readStringUntil('\n');
-    Serial.print("Received: ");
+  if (downlinkSerial.available()) {
+    String input = downlinkSerial.readStringUntil('\n');
+
+    Serial.print("Received from Uplink: ");
     Serial.println(input);
+    Serial.println("Sending ACK.");
+
+    downlinkSerial.println("MESSAGE ACK");
   }
 
   delay(100);
